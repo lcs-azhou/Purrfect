f14dea5eb93d77f27ec80549ba67b972cd26c5f6
Ansheng Zhou <azhou@lcs.on.ca>
2024-07-04 21:20:43 +0800

    Initial Commit

---
+++ Purrfect/PurrfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurrfectApp.swift
+//  Purrfect
+//
+//  Created by Ansheng Zhou on 2024-07-04.
+//
+
+import SwiftUI
+
+@main
+struct PurrfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
